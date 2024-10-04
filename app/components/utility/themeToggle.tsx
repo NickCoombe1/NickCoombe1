04d94d7703acbeb4c2b461c73ff20370f1e41f24
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-10-04 15:04:32 +0100

    update toggle to be not use theme

--- app/components/utility/themeToggle.tsx
+++ app/components/utility/themeToggle.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode, useEffect, useState } from "react";
 import { Button } from "@headlessui/react";
+import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
 
 export default function ThemeToggle(): ReactNode {
   const [theme, setTheme] = useState<string | null>(null);
@@ -17,6 +18,7 @@ export default function ThemeToggle(): ReactNode {
     setTheme(initialTheme);
     root.classList.add(initialTheme);
   }, []);
+
   const toggleTheme = () => {
     const root = window.document.documentElement;
     const newTheme = theme === "light" ? "dark" : "light";
@@ -26,13 +28,17 @@ export default function ThemeToggle(): ReactNode {
     localStorage.setItem("theme", newTheme);
     setTheme(newTheme);
   };
-  //TODO change to icon
+
   return (
     <Button
       className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
       onClick={toggleTheme}
     >
-      {`Switch to ${theme === "light" ? "Dark" : "Light"} Mode`}
+      <MoonIcon className="h-5 w-5 dark:hidden" aria-hidden="true" />
+      <SunIcon className="h-5 w-5 hidden dark:block" aria-hidden="true" />
+      <span className="hidden md:inline">
+        {`Switch to ${theme === "light" ? "Dark" : "Light"} Mode`}
+      </span>
     </Button>
   );
 }
