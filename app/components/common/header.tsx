e19e5aa01f0749231d7c86b66d79076ac2467420
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-10-04 15:24:00 +0100

    Update header font color

--- app/components/common/header.tsx
+++ app/components/common/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="flex w-full justify-between p-6 items-center">
-      <div className="text-sm/6 font-semibold text-white/50">
+      <div className="text-sm/6 font-semibold">
         {" "}
         <Link href="/about">About Us</Link>
       </div>
