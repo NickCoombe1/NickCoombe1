fd54356e02d1e0c21a9c9242ea0bee1e196fad26
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-10-04 14:13:12 +0100

    Add light mode toggle

--- app/components/readMe.tsx
+++ app/components/readMe.tsx
@@ -4,7 +4,7 @@ import markdownContent from "../../README.md";
 import ReactMarkdown from "react-markdown";
 export default function ReadMe() {
   return (
-    <div>
+    <div className="bg-white dark:bg-slate-800">
       <ReactMarkdown>{markdownContent}</ReactMarkdown>
     </div>
   );
--- app/components/utility/themeToggle.tsx
+++ app/components/utility/themeToggle.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ReactNode, useEffect, useState } from "react";
+import { Button } from "@headlessui/react";
 
 export default function ThemeToggle(): ReactNode {
   const [theme, setTheme] = useState<string | null>(null);
@@ -27,11 +28,11 @@ export default function ThemeToggle(): ReactNode {
   };
   //TODO change to icon
   return (
-    <button
+    <Button
+      className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white"
       onClick={toggleTheme}
-      className="bg-gray-200 dark:bg-gray-800 p-2 rounded"
     >
-      {theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
-    </button>
+      {`Switch to ${theme === "light" ? "Dark" : "Light"} Mode`}
+    </Button>
   );
 }
