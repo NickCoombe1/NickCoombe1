65f6bd17ed26390e26d02de052c0bbf63e78b3d2
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-11-01 15:18:52 +0000

    tidy up page component

---
+++ app/api/fetchFPLData.ts
@@ -0,0 +1,89 @@
+import {
+  FplBootstrapResponse,
+  FplTeamPicksResponse,
+  PlayerDataResponse,
+  PlayerPick,
+} from "@/app/models/scoring";
+
+export default async function fetchFplData(): Promise<
+  {
+    teamID: number;
+    picks: PlayerPick[];
+  }[]
+> {
+  const teamIDs = [401955, 406387];
+  const gameweek = 9;
+
+  try {
+    return await Promise.all(
+      teamIDs.map(async (teamID) => {
+        const scoringRequest = await fetch(
+          `https://draft.premierleague.com/api/event/${gameweek}/live`,
+        );
+        const response = await fetch(
+          `https://draft.premierleague.com/api/entry/${teamID}/event/${gameweek}`,
+          {
+            next: { revalidate: 60 },
+          },
+        );
+        const bootStrap = await fetch(
+          `https://draft.premierleague.com/api/bootstrap-static`,
+        );
+        if (!response.ok) {
+          throw new Error(
+            `Failed to fetch data for team ${teamID}: ${response.statusText}`,
+          );
+        }
+
+        const teamData: FplTeamPicksResponse = await response.json();
+        const scoringData: PlayerDataResponse = await scoringRequest.json();
+        const bootStrapData: FplBootstrapResponse = await bootStrap.json();
+
+        const picksWithPoints = mapBootstrapData(
+          bootStrapData,
+          scoringData,
+          teamData,
+        );
+        console.log(picksWithPoints);
+        return {
+          teamID,
+          picks: picksWithPoints,
+        };
+      }),
+    );
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+function mapBootstrapData(
+  FplBootstrapResponse: FplBootstrapResponse,
+  scoringData: PlayerDataResponse,
+  teamData: FplTeamPicksResponse,
+) {
+  return teamData.picks.map((pick) => {
+    //find player's points for the week
+
+    const playerData = scoringData.elements[pick.element];
+    const basePoints = playerData?.stats.total_points || 0;
+    const totalPoints = basePoints * pick.multiplier;
+    let playerName;
+    //find player's info
+    const playerInfo = Object.values(FplBootstrapResponse.elements).find(
+      (player) => player.id === pick.element,
+    );
+    if (playerInfo === undefined) {
+      playerName = "Unknown";
+    } else {
+      playerName = playerInfo.web_name;
+    }
+
+    return {
+      ...pick,
+      points: totalPoints,
+      pointDetails: playerData?.explain,
+      name: playerName,
+    };
+  });
+}
--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -1,99 +1,14 @@
 import React from "react";
 import ScoreBoard from "../components/scoring/scoreboard";
-import {
-  FplBootstrapResponse,
-  FplTeamPicksResponse,
-  PlayerDataResponse,
-  PlayerPick,
-} from "@/app/models/scoring";
+import fetchFplData from "@/app/api/fetchFPLData";
 
-async function fetchFplData(): Promise<
-  {
-    teamID: number;
-    picks: PlayerPick[];
-  }[]
-> {
-  const teamIDs = [401955, 406387];
-  const gameweek = 9;
-
-  try {
-    return await Promise.all(
-      teamIDs.map(async (teamID) => {
-        const scoringRequest = await fetch(
-          `https://draft.premierleague.com/api/event/${gameweek}/live`,
-        );
-        const response = await fetch(
-          `https://draft.premierleague.com/api/entry/${teamID}/event/${gameweek}`,
-          {
-            next: { revalidate: 60 },
-          },
-        );
-        const bootStrap = await fetch(
-          `https://draft.premierleague.com/api/bootstrap-static`,
-        );
-        if (!response.ok) {
-          throw new Error(
-            `Failed to fetch data for team ${teamID}: ${response.statusText}`,
-          );
-        }
-
-        const teamData: FplTeamPicksResponse = await response.json();
-        const scoringData: PlayerDataResponse = await scoringRequest.json();
-        const bootStrapData: FplBootstrapResponse = await bootStrap.json();
-
-        const picksWithPoints = mapBootstrapData(
-          bootStrapData,
-          scoringData,
-          teamData,
-        );
-        console.log(picksWithPoints);
-        return {
-          teamID,
-          picks: picksWithPoints,
-        };
-      }),
-    );
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-}
-
-function mapBootstrapData(
-  FplBootstrapResponse: FplBootstrapResponse,
-  scoringData: PlayerDataResponse,
-  teamData: FplTeamPicksResponse,
-) {
-  return teamData.picks.map((pick) => {
-    //find player's points for the week
-
-    const playerData = scoringData.elements[pick.element];
-    const basePoints = playerData?.stats.total_points || 0;
-    const totalPoints = basePoints * pick.multiplier;
-    let playerName;
-    //find player's info
-    const playerInfo = Object.values(FplBootstrapResponse.elements).find(
-      (player) => player.id === pick.element,
-    );
-    if (playerInfo === undefined) {
-      playerName = "Unknown";
-    } else {
-      playerName = playerInfo.web_name;
-    }
-
-    return {
-      ...pick,
-      points: totalPoints,
-      pointDetails: playerData?.explain,
-      name: playerName,
-    };
-  });
-}
 export default async function Scoring() {
-  const teamsData = await fetchFplData();
-
+  const [teamsData] = await Promise.all([fetchFplData()]);
   return (
     <div className="min-h-screen gap-16 flex justify-evenly">
+      <button className="lg:hidden mb-4 px-4 py-2 bg-blue-500 text-white rounded-lg">
+        Toggle Scoreboard
+      </button>
       {teamsData.map((team) => (
         <ScoreBoard key={team.teamID} teamID={team.teamID} picks={team.picks} />
       ))}
