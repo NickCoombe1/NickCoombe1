2efa455845f38443db88bfbcfd76b486ff947906
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-11-20 18:21:10 +0000

    minor styling

--- app/scoring/[leagueID]/[teamID]/[teamIndex]/page.tsx
+++ app/scoring/[leagueID]/[teamID]/[teamIndex]/page.tsx
@@ -50,7 +50,6 @@ export default async function ScoringPage({
         teamIDs.push(team2ID);
       }
     }
-    console.log(teamIDs);
 
     teamsData = await fetchFplData(currentGameweek, teamIDs);
   }
@@ -73,23 +72,29 @@ export default async function ScoringPage({
   }
 
   return (
-    <div className="min-h-screen flex flex-col gap-2">
-      <div className="flex justify-center lg:hidden">
-        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            <a href={`/scoring/${leagueID}/${teamID}/${nextTeamIndex}`}>
-              Other Team
-            </a>
-          </span>
-        </button>
+    <div className="min-h-screen flex flex-col gap-4">
+      <div
+        className={
+          "flex justify-center gap-4 align-center items-center flex-col"
+        }
+      >
+        <div className="text-3xl">Gameweek {gameweekInfo?.current_event}</div>
+        <div className="flex justify-center lg:hidden mb-2">
+          <button className="relative inline-flex items-center justify-center p-0.5  overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span className="relative px-5 py-2.5 items-center transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              <a href={`/scoring/${leagueID}/${teamID}/${nextTeamIndex}`}>
+                Other Team
+              </a>
+            </span>
+          </button>
+        </div>
       </div>
-      <div className=" mb-4">Gameweek {gameweekInfo?.current_event}</div>
-      <div className="min-h-screen gap-16 flex justify-evenly">
+      <div className="gap-8 flex justify-evenly items-center">
         {teamsData &&
           teamsData.map((team, index) => (
             <div
               key={team.teamID}
-              className={`${teamIndex === index ? "block" : "hidden lg:block"} w-full max-w-md mx-2`}
+              className={`${teamIndex === index ? "block" : "hidden lg:block"} w-full max-w-md `}
             >
               <ScoreBoard picks={team.picks} team={teams[index]} />
             </div>
