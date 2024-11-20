1fb3118f05b70b20886f27292e2bbd133f88cc7b
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-11-20 18:08:45 +0000

    Fix team check

--- app/scoring/[leagueID]/[teamID]/[teamIndex]/page.tsx
+++ app/scoring/[leagueID]/[teamID]/[teamIndex]/page.tsx
@@ -50,6 +50,7 @@ export default async function ScoringPage({
         teamIDs.push(team2ID);
       }
     }
+    console.log(teamIDs);
 
     teamsData = await fetchFplData(currentGameweek, teamIDs);
   }
@@ -76,7 +77,9 @@ export default async function ScoringPage({
       <div className="flex justify-center lg:hidden">
         <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            <a href={`/scoring/${nextTeamIndex}`}>Check Other Team</a>
+            <a href={`/scoring/${leagueID}/${teamID}/${nextTeamIndex}`}>
+              Other Team
+            </a>
           </span>
         </button>
       </div>
