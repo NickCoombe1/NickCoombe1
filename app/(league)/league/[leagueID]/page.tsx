c09e44b4595cacab2d114e512b263cd939ab857f
NickCoombe1 <Nick.Coombe@red-gate.com>
2024-11-29 16:52:36 +0000

    add refresh button

--- app/(league)/league/[leagueID]/page.tsx
+++ app/(league)/league/[leagueID]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-
+import React, { useCallback, useEffect, useState } from "react";
 import { LeagueData } from "@/app/models/league";
 import { GameStatusData } from "@/app/models/game";
 import { ScoringData } from "@/app/api/fetchScoringData/route";
@@ -12,6 +11,8 @@ import {
   fetchTeamDetails,
 } from "@/app/apiHelpers/apiHelpers";
 import MatchUpCard from "@/app/components/scoring/matchup";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function LeaguePage({
   params,
@@ -27,54 +28,54 @@ export default function LeaguePage({
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const leagueID = Number(params.leagueID);
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const gameweekResponse = await fetchGameWeekDetails();
+      const leagueResponse = await fetchLeagueData(Number(params.leagueID));
 
-    if (isNaN(leagueID)) {
-      setError("Invalid leagueID: must be a number");
-      return;
-    }
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const gameweekResponse = await fetchGameWeekDetails();
-        const leagueResponse = await fetchLeagueData(leagueID);
-
-        if (!gameweekResponse || !leagueResponse) {
-          setError("Failed to load gameweek or league data");
-          return;
-        }
-
-        setGameweekInfo(gameweekResponse);
-        setLeagueData(leagueResponse);
-
-        const currentGameweek = gameweekResponse.current_event;
-        if (!currentGameweek) return;
-
-        if (currentGameweek) {
-          for (const team of leagueResponse.league_entries) {
-            const teamsResponse = await fetchTeamDetails(
-              team.entry_id,
-              currentGameweek,
-            );
-            if (teamsResponse) {
-              setTeamsScoringData((prev) => ({
-                ...prev,
-                [team.id]: teamsResponse,
-              }));
-            }
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setError("An unexpected error occurred.");
-      } finally {
-        setLoading(false);
+      if (!gameweekResponse || !leagueResponse) {
+        setError("Failed to load gameweek or league data");
+        return;
       }
-    };
 
-    fetchData();
+      setGameweekInfo(gameweekResponse);
+      setLeagueData(leagueResponse);
+
+      const currentGameweek = gameweekResponse.current_event;
+      if (!currentGameweek) return;
+
+      for (const team of leagueResponse.league_entries) {
+        const teamsResponse = await fetchTeamDetails(
+          team.entry_id,
+          currentGameweek,
+        );
+        if (teamsResponse) {
+          setTeamsScoringData((prev) => ({
+            ...prev,
+            [team.id]: teamsResponse,
+          }));
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("An unexpected error occurred.");
+    } finally {
+      setLoading(false);
+    }
   }, [params.leagueID]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData, params.leagueID]);
+
+  const handleRefresh = () => {
+    setGameweekInfo(null);
+    setLeagueData(null);
+    setTeamsScoringData(null);
+    setError("");
+    fetchData();
+  };
 
   if (error) {
     return (
@@ -91,14 +92,22 @@ export default function LeaguePage({
       </div>
     );
   }
+
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-800 text-gray-900 dark:text-gray-100 flex flex-col gap-6">
       {teamsScoringData && (
         <>
-          <div className="flex flex-col items-center gap-4">
+          <div className="flex items-center justify-center gap-4 w-full">
             <h1 className="text-3xl font-bold">
               Gameweek {gameweekInfo?.current_event}
             </h1>
+            <button
+              onClick={handleRefresh}
+              className="top-4 right-4 p-2 bg-indigo-600 text-white rounded-full hover:bg-indigo-500 transition"
+              aria-label="Refresh"
+            >
+              <FontAwesomeIcon icon={faSyncAlt} />
+            </button>
           </div>
 
           <div className="w-full ">
@@ -144,7 +153,7 @@ export default function LeaguePage({
                           team: team2,
                           totalPoints: match.league_entry_2_points,
                         }}
-                      />{" "}
+                      />
                     </div>
                   );
                 })}
