6d5fbfb9a50509639731f59ca8cdf3f126233772
NickCoombe1 <51348672+NickCoombe1@users.noreply.github.com>
2024-12-08 15:17:21 +0000

    remove files

--- app/models/fplTeamPicksResponse.ts
+++
@@ -1,7 +0,0 @@
-import { PlayerPick } from "./playerPick";
-
-export interface FplTeamPicksResponse {
-  picks: PlayerPick[];
-  entry_history: Record<string, any>;
-  subs: any[];
-}
--- app/models/game.ts
+++
@@ -1,8 +0,0 @@
-export interface GameStatusData {
-  current_event: number;
-  current_event_finished: boolean;
-  next_event: number;
-  processing_status: string;
-  trades_time_for_approval: boolean;
-  waivers_processed: boolean;
-}
